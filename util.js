73262b6a6681898a593ad9b73147b5ccb991c05e
Krystal <409329578@qq.com>
2016-01-26 00:06:05 +0800

    Update util.js

--- util.js
+++ util.js
@@ -3,7 +3,7 @@ var arr=new Array();
 function isArray(arr){
    var arr=[1,2,3];
 }
-   alert（typeof arr）;// typeof操作符可以检测Function，String、Number、Undefined类型，而Array类型则返回"object"
+   alert(typeof arr);// typeof操作符可以检测Function，String、Number、Undefined类型，而Array类型则返回"object"
    alert(arr instanceof Array);   //方法1：instanceof操作符。返回true
-   alert（arr.constructor == Array）;//方法2：对象的constructor属性。返回true
-   alert（object.prototype.toString.call(arr)=="[object Array]"）;// 方法3：可以检测出所有类型,返回true。
+   alert(arr.constructor == Array);//方法2：对象的constructor属性。返回true
+   alert(object.prototype.toString.call(arr)=="[object Array]");// 方法3：可以检测出所有类型,返回true。
